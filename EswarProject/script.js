b6fc2eec39f0697302631f6206c7b25faa7d1c10
Eswar Jakkula <7674934680esu@gmail.com>
2025-03-13 16:43:35 +0530

    Add files via upload

---
+++ EswarProject/script.js
@@ -0,0 +1,8 @@
+const MyButton=document.getElementById("Button")
+  
+MyButton.addEventListener('click',function(button){
+    button.preventDefault();
+
+   MyButton.style.backgroundColor="red"
+
+})
